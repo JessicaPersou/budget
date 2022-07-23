4cf32adbd40489ea25e0973dc73f037746ad3cb9
JessicaPersou <jessica.sousa@koin.com.br>
2022-07-22 21:04:50 -0300

    Métodos Get e Post não estão funcionando

--- src/main/java/com/example/algamoney/api/resource/PessoaResource.java
+++ src/main/java/com/example/algamoney/api/resource/PessoaResource.java
@@ -3,11 +3,15 @@ package com.example.algamoney.api.resource;
 import com.example.algamoney.api.model.Pessoa;
 import com.example.algamoney.api.repository.PessoaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
+import java.net.URI;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/pessoa")
@@ -16,8 +20,19 @@ public class PessoaResource {
     @Autowired
     private PessoaRepository pessoaRepository;
 
-    @GetMapping
-    public List<Pessoa> listar(){
-        return pessoaRepository.findAll();
+    @PostMapping
+    public ResponseEntity<Pessoa> criarPessoa(@Valid @RequestBody Pessoa pessoa, HttpServletResponse response){
+        Pessoa salvaPessoa = pessoaRepository.save(pessoa);
+
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{idPessoa}")
+                .buildAndExpand(salvaPessoa.getIdPessoa()).toUri();
+        response.setHeader("Location", uri.toASCIIString());
+        return ResponseEntity.created(uri).body(salvaPessoa);
+    }
+
+    @GetMapping("/{idPessoa}")
+    public ResponseEntity<Pessoa> buscarPorIdPessoa(@PathVariable Long idPessoa){
+        Optional<Pessoa> pessoa = pessoaRepository.findById(idPessoa);
+        return pessoa.isPresent() ?ResponseEntity.ok(pessoa.get()) : ResponseEntity.notFound().build();
     }
 }
