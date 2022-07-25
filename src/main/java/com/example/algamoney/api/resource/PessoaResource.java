8cedeb4af563c81336606cc15e6e2c239fae0a57
JessicaPersou <jessica.sousa@koin.com.br>
2022-07-25 10:55:38 -0300

    Funcionando Métodos Get e Post

--- src/main/java/com/example/algamoney/api/resource/PessoaResource.java
+++ src/main/java/com/example/algamoney/api/resource/PessoaResource.java
@@ -1,15 +1,17 @@
 package com.example.algamoney.api.resource;
 
+
 import com.example.algamoney.api.model.Pessoa;
 import com.example.algamoney.api.repository.PessoaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 import java.net.URI;
-
+import java.util.List;
 
 @RestController
 @RequestMapping("/pessoa")
@@ -17,6 +19,11 @@ public class PessoaResource {
 
     @Autowired
     private PessoaRepository pessoaRepository;
+
+    @GetMapping
+    public List<Pessoa> listarTodos(){
+        return pessoaRepository.findAll();
+    }
 
     @PostMapping
     public ResponseEntity<Pessoa> criarPessoa(@Valid @RequestBody Pessoa pessoa, HttpServletResponse response){
@@ -29,7 +36,7 @@ public class PessoaResource {
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<Pessoa> buscarPorIdPessoa(@PathVariable Long id){
+    public ResponseEntity<Pessoa> buscarPorId(@PathVariable Long id){
         return this.pessoaRepository.findById(id)
                 .map(pessoa -> ResponseEntity.ok(pessoa))
                 .orElse(ResponseEntity.notFound().build());
